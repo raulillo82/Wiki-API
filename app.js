b6d312b1eb54cc2074a2328bac18f74c23568790
Raúl Osuna <raulosuna@gmail.com>
2022-03-29 16:28:44 +0200

    Implement PATCH method to /articles/XXX route for article update operations

--- app.js
+++ app.js
@@ -99,6 +99,23 @@ app.route("/articles/:articleTitle")
       }
     );
 
+  })
+
+  .patch(function(req, res) {
+    Article.updateOne({
+        title: req.params.articleTitle
+      }, {
+        $set: req.body
+      },
+      function(err) {
+        if (!err) {
+          res.send("Successfully updated article.");
+        } else {
+          res.send(err);
+        }
+      }
+
+    );
   });
 
 
