72615bd825b27484788ef4d1bd054fa33ac4745e
Raúl Osuna <raulosuna@gmail.com>
2022-03-29 15:18:25 +0200

    Refactor code to use app.route method chaining in it all existing methods so far: GET, POST and DELETE

--- app.js
+++ app.js
@@ -24,41 +24,47 @@ const articleSchema = {
 const Article = mongoose.model("Article", articleSchema);
 
 
-app.get("/articles", function(req, res) {
-  Article.find(function(err, foundArticles) {
-    if (!err) {
-      //console.log(foundArticles);
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
-  });
-});
+//articles route
+app.route("/articles")
 
-app.post("/articles", function(req, res) {
-  const newArticle = new Article ({
-    title: req.body.title,
-    content: req.body.content
-  });
-  newArticle.save(function(err){
-    if (!err){
-      res.send("Successfully added a new article.");
-    } else {
-      res.send(err);
-    }
-  });
-});
+  //GET method
+  .get(function(req, res) {
+    Article.find(function(err, foundArticles) {
+      if (!err) {
+        //console.log(foundArticles);
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
 
-app.delete("/articles", function(req, res) {
-  //delete everything
-  Article.deleteMany(function(err){
-    if (!err){
-      res.send("Successfully deleted all articles.");
-    } else {
-      res.send(err);
-    }
+  //POST method
+  .post(function(req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content
+    });
+    newArticle.save(function(err) {
+      if (!err) {
+        res.send("Successfully added a new article.");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+
+  //DELETE method
+  .delete(function(req, res) {
+    //delete everything
+    Article.deleteMany(function(err) {
+      if (!err) {
+        res.send("Successfully deleted all articles.");
+      } else {
+        res.send(err);
+      }
+    });
   });
-});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
